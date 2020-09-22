8ced972cf1e95c5dd87fe05ac247ebcbce418d91
Havanmai <71115868+Havanmai@users.noreply.github.com>
2020-09-22 12:26:32 +0700

    bo sung ui

--- MISA.CukCuk/MISA.CukCuk/wwwroot/js/customer.js
+++ MISA.CukCuk/MISA.CukCuk/wwwroot/js/customer.js
@@ -1,3 +1,13 @@
 ﻿$(document).ready(function () {
-    alert('hi');
+    $('.modal-content').hide();
+})
+$(document).ready(function () {
+    $('#btnthem').click(function () {
+        $('.modal-content').show();
+        //$('.content').css('opacity') = '0.4';
+        $('.content').css('opacity', '0.4');
+        $('.menu').css('opacity', '0.4');
+        //$('.menu').css('opacity') = '0.4';
+        
+    })
 })
