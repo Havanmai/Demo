aa17863c2faeb4f43bdbb626d8ae3474e5594d14
HA DO THU <dtha@rd.local>
2020-09-23 09:07:36 +0700

    bo sung

--- MISA.CukCuk/MISA.CukCuk/wwwroot/js/customer.js
+++ MISA.CukCuk/MISA.CukCuk/wwwroot/js/customer.js
@@ -1,13 +1,15 @@
 ﻿$(document).ready(function () {
     $('.modal-content').hide();
+    $('.modal').hide();
 })
 $(document).ready(function () {
     $('#btnthem').click(function () {
+        $('.modal').show();
+        //$('.modal').css('opacity', '0.4');
         $('.modal-content').show();
-        //$('.content').css('opacity') = '0.4';
-        $('.content').css('opacity', '0.4');
-        $('.menu').css('opacity', '0.4');
-        //$('.menu').css('opacity') = '0.4';
+        
+//        //$('#mainfull').css('opacity', '0.4');
+//        //$('#mainfull').css('position', 'relative');
         
     })
 })
